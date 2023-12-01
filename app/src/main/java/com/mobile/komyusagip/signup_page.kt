d6dd4ba3f4e1cece40bdf1407b80d76d71ce8e49
username <email>
2023-12-01 10:06:31 +0800

    initial layout for signup

---
+++ app/src/main/java/com/mobile/komyusagip/signup_page.kt
@@ -0,0 +1,11 @@
+package com.mobile.komyusagip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signup_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_layout)
+    }
+}
