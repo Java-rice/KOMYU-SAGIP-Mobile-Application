e36d8d93f82a7ef8e1095ea6824c148c5f8e5b56
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2023-12-04 01:25:42 +0800

    splash screen and startup page, sdk updated to min:31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mobile.komyusagip"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
