5adea40720d7d0688b21f821bdbf16f6a254eace
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2023-12-04 16:32:05 +0800

    splash screen resolved: no need to update min sdk from 28 to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mobile.komyusagip"
-        minSdk = 31
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -41,4 +41,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
--- app/src/main/java/com/mobile/komyusagip/MainActivity.kt
+++ app/src/main/java/com/mobile/komyusagip/MainActivity.kt
@@ -2,10 +2,15 @@ package com.mobile.komyusagip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { false }
         setContentView(R.layout.activity_main)
+
     }
 }
