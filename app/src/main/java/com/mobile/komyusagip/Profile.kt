ef207e7ff8191acb587939006e64b23431ffff3e
username <email>
2023-12-20 16:46:29 +0800

    profile (no divider yet)

---
+++ app/src/main/java/com/mobile/komyusagip/Profile.kt
@@ -0,0 +1,10 @@
+package com.mobile.komyusagip
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
