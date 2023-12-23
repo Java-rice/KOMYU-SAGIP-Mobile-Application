995df5287e6924f3444b73099f3b594bf017542f
username <email>
2023-12-23 15:16:36 +0800

    CreatePost Added

--- app/src/main/java/com/mobile/komyusagip/Profile.kt
+++ app/src/main/java/com/mobile/komyusagip/Profile.kt
@@ -1,6 +1,9 @@
 package com.mobile.komyusagip
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
