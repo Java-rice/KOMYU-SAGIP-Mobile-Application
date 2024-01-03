b99ef894cb2cc40dca9791fec3c17aa04b464535
Java-rice <johnmarkpperoche@iskolarngbayan.pup.edu.ph>
2024-01-03 11:50:07 +0800

    working bottom navigation bottom with fragment pages

--- app/src/main/java/com/mobile/komyusagip/Home.kt
+++ app/src/main/java/com/mobile/komyusagip/Home.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import com.mobile.komyusagip.databinding.ActivityMainBinding
+import com.mobile.komyusagip.databinding.ActivityHomeBinding
 
 class Home  : AppCompatActivity(){
     private lateinit var fragmentManager: FragmentManager
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.bottomNavigation.background = null
