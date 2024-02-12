9c359a0c42a5f2c52a0163dc18f42dc6d83f121e
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2024-02-12 15:20:18 +0800

    added picasso dependency, changed from imageview -> circleimageview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
